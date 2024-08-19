25435c83a0ed334ea4ef05f4d1ad1f5364131f68
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-19 22:13:29 +0700

    creat Conversations ui

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -1,8 +1,20 @@
 export default function Conversation() {
   return (
     <>
-      <div className="flex items-center gap-2 p-2 py-1 rounded cursor-pointer hover:bg-sky-500"></div>
-      <div className=""></div>
+      <div className="flex items-center gap-2 p-2 py-1 rounded cursor-pointer hover:bg-sky-500">
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src="https://i.pravatar.cc/300" alt="user avatar" />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex justify-between gap-3">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl">😽</span>
+          </div>
+        </div>
+      </div>
+      <div className="h-1 py-0 my-0 divider"></div>
     </>
   );
 }
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
+import Conversations from "./Conversations";
 import SearchInput from "./SearchInput";
 
 export default function Sidebar() {
   return (
     <div>
-        <SearchInput/>
-        <div className="px-3 divider"></div>
-        {/* <Conversations/>
-        <LogOutButton/> */}
+      <SearchInput />
+      <div className="px-3 divider"></div>
+      <Conversations />
+
+      <LogOutButton />
     </div>
-  )
+  );
 }
