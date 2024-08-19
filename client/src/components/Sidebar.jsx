e43fa1d8cf7cc254911d413093d6a0f559cbbc1e
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-19 22:17:12 +0700

    create logoutBtn ui successfully

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import Conversations from "./Conversations";
+import LogOutButton from "./LogOutButton";
 import SearchInput from "./SearchInput";
 
 export default function Sidebar() {
   return (
-    <div>
+    <div className="flex flex-col p-4 border-r border-slate-500">
       <SearchInput />
       <div className="px-3 divider"></div>
       <Conversations />
