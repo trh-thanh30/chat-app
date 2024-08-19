3652cd67a2a8bb0dc9c186cc88620933e4c42fbe
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-19 16:20:48 +0700

    create verifyToken route

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.models");
 const bcryptjs = require("bcryptjs");
+const verifyToken = require("../utils/generateToken");
 const logIn = async (req, res) => {};
 const signUp = async (req, res) => {
   try {
@@ -21,13 +22,19 @@ const signUp = async (req, res) => {
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
-    await newUser.save();
-    res.status(200).json({
-      _id: newUser._id,
-      username: newUser.username,
-      fullName: newUser.fullName,
-      profilePic: newUser.profilePic,
-    });
+
+    if (newUser) {
+      verifyToken(newUser._id, res);
+      await newUser.save();
+      res.status(200).json({
+        _id: newUser._id,
+        username: newUser.username,
+        fullName: newUser.fullName,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
---
+++ server/utils/generateToken.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+const verifyToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+  res.cookie("access_token", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
+};
+
+module.exports = verifyToken;
