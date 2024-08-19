74118064a4d4b3cf5012d7f5b7fe74ad580899a2
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-20 00:47:24 +0700

    create logout hook successfully

--- client/src/components/LogOutButton.jsx
+++ client/src/components/LogOutButton.jsx
@@ -1,8 +1,17 @@
 import { BiLogOut } from "react-icons/bi";
+import useLogOut from "../hooks/useLogOut";
 export default function LogOutButton() {
+  const { loading, logout } = useLogOut();
   return (
     <div className="mt-auto">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer"></BiLogOut>
+      {!loading ? (
+        <BiLogOut
+          onClick={logout}
+          className="w-6 h-6 text-white cursor-pointer"
+        ></BiLogOut>
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
   );
 }
